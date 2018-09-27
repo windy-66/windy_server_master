18b43250f0bba4d78e74c976794de61b5ad9b954
909633131@qq.com <zuoge85@gmail.com>
2018-09-28 00:20:03 +0800

    游戏大厅服务器项目模板

---
+++ game-hall/src/main/java/com/windy/game/hall/GameHall.java
@@ -0,0 +1,20 @@
+package com.windy.game.hall;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 游戏大厅服务器启动类
+ *
+ * @author jiaxiaocheng
+ * @create 2018-09-28 0:03
+ **/
+public class GameHall {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GameHall.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("gamehall start");
+    }
+}
