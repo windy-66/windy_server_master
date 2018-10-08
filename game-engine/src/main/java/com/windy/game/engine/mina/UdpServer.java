e9d32af72c7fd9c31dbf71b9658ec8dd2dc4b861
909633131@qq.com <zuoge85@gmail.com>
2018-10-08 23:35:38 +0800

    udp server

--- game-engine/src/main/java/com/windy/game/engine/mina/UdpServer.java
+++ game-engine/src/main/java/com/windy/game/engine/mina/UdpServer.java
@@ -1,5 +1,11 @@
 package com.windy.game.engine.mina;
 
+import org.apache.mina.core.service.IoHandler;
+import org.apache.mina.transport.socket.nio.NioDatagramAcceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.windy.game.engine.mina.config.MinaServerConfig;
 /**
  * UDP 服务器
  *
@@ -7,5 +13,20 @@ package com.windy.game.engine.mina;
  * @create 2018-10-08 17:40
  **/
 public class UdpServer implements Runnable{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UdpServer.class);
+    private MinaServerConfig minaServerConfig;
+    private IoHandler ioHandler;
+    private NioDatagramAcceptor acceptor;
+    public UdpServer(MinaServerConfig minaServerConfig, IoHandler ioHandler) {
+        super();
+        this.minaServerConfig = minaServerConfig;
+        this.ioHandler = ioHandler;
+        acceptor = new NioDatagramAcceptor();
+    }
+    @Override
+    public void run() {
+
+    }
 
 }
---
+++ game-engine/src/main/java/com/windy/game/engine/mina/config/MinaServerConfig.java
@@ -0,0 +1,13 @@
+package com.windy.game.engine.mina.config;
+
+import com.windy.game.engine.server.BaseServerConfig;
+
+/**
+ * mina server 配置文件
+ *
+ * @author jiaxiaocheng
+ * @create 2018-10-08 23:09
+ **/
+public class MinaServerConfig extends BaseServerConfig{
+
+}
